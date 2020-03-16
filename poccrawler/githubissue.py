1887e639c168585b9220592ac37e3c45e1781e26
zjuchenyuan <chenyuan@zju.edu.cn>
2020-03-16 15:07:00 +0800

    [poccrawler] support github issue body

--- poccrawler/githubissue.py
+++ poccrawler/githubissue.py
@@ -6,19 +6,27 @@ _url_re = re.compile(r'(?im)((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4
 def getissuetext(url):
     # return list of issue comments
     # apiurl = "https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}/comments"
+    res = []
+    apiurl = url.replace("/github.com/", "/api.github.com/repos/")
+    x = a.get(apiurl, result=False, o=True, cache=True).json()
+    res.append(x["body"])
     apiurl = url.replace("/github.com/", "/api.github.com/repos/")+"/comments"
     x = a.get(apiurl, result=False, o=True, cache=True).json()
-    return [i["body"] for i in x]
+    res.extend(i["body"] for i in x)
+    #print(res)
+    return res
 
 def extracturls(text):
+    #print(text)
     return _url_re.findall(text)
 
 def ispocurl(url):
-    return "poc" in url
+    #print(url)
+    return "poc" in url or "?raw=true" in url
 
 def getpocurls(url):
     return set([i for i in extracturls("\n".join(getissuetext(url))) if ispocurl(i)])
 
 if __name__ == "__main__":
     from pprint import pprint
-    pprint(getpocurls("https://github.com/Exiv2/exiv2/issues/428"))
+    pprint(getpocurls("https://github.com/Exiv2/exiv2/issues/712"))
